bacba530a2a98f63bc164c8604b5f14f5a6928fe
ailiang <519287479@qq.com>
2021-03-19 04:52:34 -0700

    integration db

--- server/main.c
+++ server/main.c
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "server.h"
+#include "db.h"
 
 int main()
 {
+	//init db
+	Mdb db;
+	db.CreateTable("test1");
+	db.CreateTable("test2");
+	
 	muduo::net::EventLoop loop;
 	muduo::net::InetAddress addr(9999);
 	Server serv( &loop, addr );
