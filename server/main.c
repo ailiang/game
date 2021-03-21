9fc4b42844c92c418a31107ed909313d5fa31f81
ailiang <ailiang.chen@hotmail.com>
2021-03-21 14:29:10 +0800

    add cpp_redis

--- server/main.c
+++ server/main.c
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "server.h"
 #include "db.h"
+#include "cpp_redis/cpp_redis"
+#include "cpp_redis/misc/macro.hpp"
 
 int main()
 {
@@ -8,7 +10,18 @@ int main()
 	Mdb db;
 	db.CreateTable("test1");
 	db.CreateTable("test2");
-	
+
+	//redis
+	cpp_redis::client re_client;
+	re_client.connect("127.0.0.1", 6379, [](const std::string& host, std::size_t port, cpp_redis::connect_state status  ){
+			if( status ==  cpp_redis::connect_state::dropped )
+			{
+			std::cout << " redis connect droped \n";
+			}
+			});
+	re_client.set("name", "cal");
+	re_client.sync_commit();
+
 	muduo::net::EventLoop loop;
 	muduo::net::InetAddress addr(9999);
 	Server serv( &loop, addr );
